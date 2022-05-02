2fc2a3498a8ec4d93cb95a6267cb233714a3849f
ketxd <kaparina.jekaterina@gmail.com>
2022-05-02 10:19:33 +0200

    Deployment refactorings

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module de.tuebingen.sfs.cldfjava {
+    exports de.tuebingen.sfs.cldfjava.data;
+    exports de.tuebingen.sfs.cldfjava.io;
+    exports de.tuebingen.sfs.cldfjava.util;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+}
